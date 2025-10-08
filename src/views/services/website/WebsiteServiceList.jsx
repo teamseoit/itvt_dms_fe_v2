80e0d219c3461bf3747a6a4c550c03303ec81414
phucleit <phucleit95@gmail.com>
2025-10-09 00:48:18 +0700

    fix

--- src/views/services/website/WebsiteServiceList.jsx
+++ src/views/services/website/WebsiteServiceList.jsx
@@ -28,7 +28,7 @@ import {
 } from '@mui/material';
 import { IconPlus, IconEdit, IconTrash, IconChevronUp, IconChevronDown, IconSearch } from '@tabler/icons-react';
 
-import WEBSITE_SERVICE_API from '../../../services/services/websiteServiceService';
+import WEBSITE_SERVICE_API from '../../../services/services/websiteService';
 import usePermissions from '../../../hooks/usePermissions';
 import { PERMISSIONS } from '../../../constants/permissions';
 import { formatDateTime, formatDate, formatPrice, maskPhoneNumber } from '../../../utils/formatConstants';
